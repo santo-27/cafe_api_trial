36fe40e85cc2d14e0e2aa54f2d390a5069221f2e
santo-27 <msantopaul@gmail.com>
2023-08-24 22:04:04 +0530

    refined commit-1(no considerable code changes)

--- main.py
+++ main.py
@@ -2,19 +2,6 @@ from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import random
 
-'''
-Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
 app = Flask(__name__)
 
 ##Connect to Database
@@ -93,39 +80,7 @@ def show_all_cafes():
     # all_cafes_list = []
     result = db.session.execute(db.select(Cafe).order_by(Cafe.name))
     all_cafes = result.scalars().all()
-    #This uses a List Comprehension but you could also split it into 3 lines.
     return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
-    for cafe in all_cafes:
-        # inner_dict = {
-        #     "can_take_calls" : cafe.can_take_calls,
-        #     "coffee_price" : cafe.coffee_price,
-        #     "has_sockets" : cafe.has_sockets,
-        #     "has_toilet" : cafe.has_toilet,
-        #     "has_wifi" : cafe.has_wifi,
-        #     "id" : cafe.id,
-        #     "img_url" : cafe.img_url,
-        #     "location" : cafe.location,
-        #     "map_url" : cafe.map_url,
-        #     "name" : cafe.name,
-        #     "seats" : cafe.seats
-        # }
-        inner_dict = cafe.to_dict()
-        all_cafes_list.append(inner_dict)
-    return jsonify(cafes=all_cafes_list)
-
-
-
-
-
-
-## HTTP GET - Read Record
-
-## HTTP POST - Create Record
-
-## HTTP PUT/PATCH - Update Record
-
-## HTTP DELETE - Delete Record
-
 
 if __name__ == '__main__':
     app.run(debug=True)
